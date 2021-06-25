9293b1e58fb6d314725e163dc5fa8062c97b9eef
Himanshu Kara <kara_himanshu@yahoo.com>
2021-06-26 01:08:37 +0530

    Sidebar fix err

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import Rightbar from "../../components/rightbar/Rightbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Topbar from "../../components/topbar/Topbar";
+import "./home.css";
+
+export default function Home() {
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
+}
